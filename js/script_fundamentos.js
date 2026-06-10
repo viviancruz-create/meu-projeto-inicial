b13d4fa2b1325babd66df9671df29d34058c00d3
Noite Cetafaju <Noite.Cetafaju@labfies.org.br>
2026-06-10 20:06:50 -0300

    demonstrado criação de variáveis

---
+++ js/script_fundamentos.js
@@ -0,0 +1,41 @@
+//cometário de uma linha
+/*
+
+cometário de múltiplas linhas
+é possível ter várias
+linha
+
+*/
+
+//comando de saída
+console.log("olá mundo!!!")
+
+//Declação de variável
+let num = 10
+var num2 = 50
+const num3 = 100
+console.log(num)
+console.log(num2)
+console.log(num3)
+
+if (true){
+    let num4 = 250
+console.log(num4)
+
+var num5 = 140
+
+}
+
+console.log(num5)
+
+num = 1882
+
+console.log(num)
+
+num2 = 58
+
+console.log(num2)
+
+num3 = 902
+
+console.log(num3)
