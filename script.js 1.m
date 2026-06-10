fbf0c6d38d3ae4bda717cbc31d8e1f5a306ee921
Vivian Suenny <vivian.cruz@aluno.senai.br>
2026-06-09 21:51:26 -0300

    projeto

---
+++ script.js 1.m
@@ -0,0 +1,7 @@
+//pegando elementos do dom
+const  formpessoa - document.querySelector
+("#formulario-pessoa")
+const divDados = document.querySelector("#div-dados")
+
+formpessoa.addEventlistener('submit' , (evt) => { 
+    evtpreventDefault())
